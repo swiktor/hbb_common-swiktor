88228b4d89e3860feaa0cdb2367b4e73c1ba2480
Szymon Wiktor <szymonwiktor+informatyk@gmail.com>
2025-03-07 11:58:18 +0100

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["10.0.254.68"];
+pub const RS_PUB_KEY: &str = "s+a3WtI8EYgdWU2X2VVpG2MfxM6BzOhZsCeA4qk7HgY=#";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
